a8b8f16d7696ae6fe42132311ceff972007f8aea
Administrator <admin@example.com>
2023-08-26 00:10:46 +0300

    added answers module_2/lesson_7

---
+++ src/module_2/lesson_7/Strings.java
@@ -0,0 +1,8 @@
+public class Strings {
+
+    public static void main(String[] args) {
+        // String hello = "Hello";
+        // String world = "World";
+        // System.out.println(hello + " " + world + "!");
+    }
+}
