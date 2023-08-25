513db59c6135b6f949593ab4cdcb2cb8585f6838
Administrator <admin@example.com>
2023-08-26 00:31:32 +0300

    added answers module_2/lesson-8

---
+++ src/module_2/lesson_8/Loop.java
@@ -0,0 +1,33 @@
+public class Loop {
+
+    public static void main(String[] args) {
+        // int i = 0;
+        // while (i <= 1000) {
+        //     if (i % 2 == 0) {
+        //         System.out.println(i);
+        //     }
+        //     i++;
+        //     if (i == 5) {
+        //         break;
+        //     }
+        // }
+
+        // int a = 0;
+        // do {
+        //     System.out.println("Hello!");            
+        // } while (a > 0);
+
+        // for (int i = 0; i < 1000; i++) {
+        //     System.out.println(i);
+        //     if (i == 10) {
+        //         break;
+        //     }
+        // }
+
+        // for (int i = 1000; i >= 0; i--) {
+        //     if (i % 3 == 0) {
+        //         System.out.println(i);
+        //     }
+        // }
+    }
+}
